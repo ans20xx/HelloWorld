f87cb26b333139f8012ec15fb7dce6df8334785a
ans20xx <2781988166@qq.com>
2021-10-22 15:10:08 +0800

    Hello world v2.0 : add date display to Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,12 @@
+import java.util.Date;
+
 public class Hello {
     public static void main(String[] args) {
+        // 获取当前时间
+        Date date = new Date(System.currentTimeMillis());
+        // 打印当前时间
+        System.out.println(date);
+        // 打印 Hello world
         System.out.println("Hello World!");
     }
 }
