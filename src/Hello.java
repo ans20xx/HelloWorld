94b2ec5d8667b08fad1fd43317a36506564d77af
ans20xx <2781988166@qq.com>
2021-10-23 11:44:14 +0800

    to #0001: add login function

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,38 @@
+import java.io.*;
+import java.net.URL;
 import java.util.Date;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class Hello {
-    public static void main(String[] args) {
-        // 获取当前时间
-        Date date = new Date(System.currentTimeMillis());
-        // 打印当前时间
-        System.out.println(date);
-        // 打印 Hello world
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        if(login()) {
+            // 获取当前时间
+            Date date = new Date(System.currentTimeMillis());
+            // 打印当前时间
+            System.out.println(date);
+            // 打印 Hello world
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("Deny....");
+        }
     }
+
+    public static boolean login() throws IOException {
+        Properties properties = new Properties();
+        URL resource = Hello.class.getClassLoader().getResource("user.properties");
+        InputStream in = new FileInputStream(resource.getPath());
+        properties.load(in);
+        System.out.print("user: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.next();
+        System.out.print("password: ");
+        String password = scanner.next();
+        if(username.equals(properties.getProperty("username")) &&
+            password.equals(properties.getProperty("password"))) {
+            return true;
+        }
+        return false;
+    }
+
 }
